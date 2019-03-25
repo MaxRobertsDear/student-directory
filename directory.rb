62a26303dce786027341373c11778edf947b7a61
MaxRobertsDear <maxhroberts@gmail.com>
2019-03-25 21:34:38 +0000

    Add cohort input

--- directory.rb
+++ directory.rb
@@ -1,29 +1,34 @@
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "For each student, please enter the name and then the cohort"
+  puts "To finish, just hit return thrice"
   students =[]
+  puts "Enter name: "
   name = gets.chomp
+  puts "Enter cohort: "
+  cohort = gets.chomp
+
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    students << {name: name, cohort: cohort}
+    puts "Now we have #{students.count} student(s)"
+    puts "Enter name: "
     name = gets.chomp
+    puts "Enter cohort: "
+    cohort = gets.chomp
   end
   students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(40)
+  puts "-------------".center(40)
 end
 def print(students)
   students.each_with_index { |student, index| 
-    if student[:name].chr == "A" && if student[:name].length < 12
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
-    end
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)".center(40)
   }
 end
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{students.count} great students".center(40)
 end
 
 students = input_students
